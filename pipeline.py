be99eb52721c081486740cb48199ea883d8e8bbb
Arkiver2 <Arkiver@hotmail.com>
2015-12-05 15:04:48 +0100

    Do not accept exit code 4. Bump version.

--- pipeline.py
+++ pipeline.py
@@ -58,7 +58,7 @@ if not WPULL_EXE:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20151205.02"
+VERSION = "20151205.03"
 TRACKER_ID = 'ftp'
 TRACKER_HOST = 'tracker.archiveteam.org'
 
@@ -246,7 +246,7 @@ pipeline = Pipeline(
     WgetDownload(
         WgetArgs(),
         max_tries=2,
-        accept_on_exit_code=[0, 4, 8],
+        accept_on_exit_code=[0, 8],
         env={
             "item_dir": ItemValue("item_dir"),
             "item_item": ItemValue("item_item"),
